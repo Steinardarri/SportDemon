593731cdb1db8d4f2c643bbecec534dd9b9c97fa
Steinar Darri Þorgilsson <steinardth@gmail.com>
2023-02-18 20:21:32 +0000

    Endurgerði Android Project

---
+++ VerkefniOgAnnad/Backup/java/is/hi/hbvg601/team16/SportDemon/MainActivity.java
@@ -0,0 +1,14 @@
+package is.hi.hbvg601.team16.SportDemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/androidTest/java/is/hi/hbvg601/team16/SportDemon/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/is/hi/hbvg601/team16/SportDemon/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("is.hi.hbvg601.team16.sportdemon", appContext.getPackageName());
+        assertEquals("is.hi.hbvg601.team16.SportDemon", appContext.getPackageName());
     }
 }
