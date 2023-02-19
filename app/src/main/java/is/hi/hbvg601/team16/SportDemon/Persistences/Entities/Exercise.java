af1cb08bb9775b4f6d3efb6ffad889eb48a7bc50
Steinar Darri Þorgilsson <steinardth@gmail.com>
2023-02-19 20:51:05 +0000

    Basic entities og rename a klasa

---
+++ app/src/main/java/is/hi/hbvg601/team16/SportDemon/Persistences/Entities/Exercise.java
@@ -0,0 +1,10 @@
+package is.hi.hbvg601.team16.SportDemon.Persistences.Entities;
+
+import java.util.*;
+
+public class Exercise {
+    private UUID ID;
+    private String title;
+    private List<String> bodyPart = new ArrayList<>();
+    private List<String> type = new ArrayList<>();
+}
