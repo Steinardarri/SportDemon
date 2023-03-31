0e37db78253e9d1a617471ae8a58bef7eb9eed90
gauti3 <arnargautio@gmail.com>
2023-03-31 15:54:25 +0000

    add exercise xml grunnur

---
+++ SportDemonApp/src/main/java/is/hi/hbvg601/team16/sportdemon/ui/homeactivity/ExerciseActivity.java
@@ -0,0 +1,13 @@
+package is.hi.hbvg601.team16.sportdemon.ui.homeactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+    }
+}
